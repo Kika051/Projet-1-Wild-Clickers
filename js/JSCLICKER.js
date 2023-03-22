c7e94147a09d526f9aa7c46cb1630163f6a1b0f6
Elodie0530 <uneondine@gmail.com>
2023-03-22 15:29:34 +0100

    ajout js pour item js

--- js/JSCLICKER.js
+++ js/JSCLICKER.js
@@ -1,6 +1,6 @@
 let points = 0
 
-function save(){
+function save() {
     alert("Votre partie est sauvegarder")
 }
 
@@ -22,16 +22,16 @@ function updateDisplay(val) {
 
 const compteur = document.querySelector('#counter-label');
 
-function miseAjourAffichagePoints(){
+function miseAjourAffichagePoints() {
     compteur.textContent = points;
 }
 
-function ajoutPoints(valeur){
+function ajoutPoints(valeur) {
     points = points + valeur;
     miseAjourAffichagePoints();
 }
 
-function retirerPoints(valeur){
+function retirerPoints(valeur) {
     points = points - valeur;
     miseAjourAffichagePoints();
 }
@@ -45,20 +45,43 @@ const prixDeveloppeurAffichage = document.querySelector('#prixDeveloppeurAfficha
 let nombreDeveloppeur = 0;
 let prixDeveloppeur = 10;
 
-developpeur.addEventListener('click', () =>{
-    if (points >=prixDeveloppeur){
+developpeur.addEventListener('click', () => {
+    if (points >= prixDeveloppeur) {
         retirerPoints(prixDeveloppeur);
         nombreDeveloppeur++;
         nbDeveloppeuraffichage.textContent = nombreDeveloppeur;
-        prixDeveloppeur = prixDeveloppeur *2;
+        prixDeveloppeur = prixDeveloppeur * 2;
         prixDeveloppeurAffichage.textContent = prixDeveloppeur;
     }
-    else{
+    else {
         alert('Pas assez de clic');
     }
 })
 
-setInterval(() =>{
+setInterval(() => {
     ajoutPoints(nombreDeveloppeur * 2);
     document.querySelector('#developpeur').textContent;
-},1000)
+}, 1000)
+
+// js Elodie item js
+
+const compteurjs = document.querySelector('#Javascript');
+let prixJavascriptAffichage = document.querySelector('#prixJavascriptAffichage').textContent;
+let nbJavascript = document.querySelector('#nbJavascript').textContent;
+
+compteurjs.addEventListener('click', function () {
+    if (points >= prixJavascriptAffichage) {
+        retirerPoints(prixJavascriptAffichage);
+        document.querySelector('#nbJavascript').textContent = ++nbJavascript;
+        prixJavascriptAffichage = prixJavascriptAffichage *2;
+        document.querySelector('#prixJavascriptAffichage').textContent = prixJavascriptAffichage;
+    }
+    else {
+        alert('il vous faut encore cliquer un peu');
+    }
+});
+
+setInterval(() => {
+    ajoutPoints(nbJavascript * 2);
+    document.querySelector('#Javascript').textContent;
+}, 1000)
